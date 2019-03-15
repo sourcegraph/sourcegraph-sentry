b8e9d2bc86cad657812c86202ca14ea0b38a3bda
Vanesa Ortiz <vanesa@sourcegraph.com>
2019-03-16 02:31:58 +0900

    refactor code to add tests

--- src/extension.test.ts
+++ src/extension.test.ts
@@ -1,22 +1,24 @@
 import expect from 'expect'
-import { getParamsFromUriPath, matchSentryProject } from './extension'
+import { getParamsFromUriPath, matchSentryProject } from './handler'
 import { Settings } from './settings'
 
 describe('getParamsFromUriPath', () => {
     it('extracts repo and file params', () =>
         expect(
             getParamsFromUriPath('git://github.com/sourcegraph/sourcegraph?264...#web/src/e2e/index.e2e.test.tsx')
-        ).toEqual([{ repo: 'sourcegraph/sourcegraph', file: 'web/src/e2e/index.e2e.test.tsx' }]))
+        ).toEqual({ repo: 'sourcegraph/sourcegraph', file: '#web/src/e2e/index.e2e.test.tsx' }))
 
     it('return empty repo if host is not GitHub', () =>
-        expect(getParamsFromUriPath('git://unknownhost.com/sourcegraph/testrepo#http/req/main.go')).toEqual([
-            { repo: '', file: 'http/req/main.go' },
-        ]))
+        expect(getParamsFromUriPath('git://unknownhost.com/sourcegraph/testrepo#http/req/main.go')).toEqual({
+            repo: '',
+            file: '#http/req/main.go',
+        }))
 
-    it('return file if document has no file format', () =>
-        expect(getParamsFromUriPath('git://github.com/sourcegraph/sourcegraph/testrepo#formatless')).toEqual([
-            { repo: 'sourcegraph/sourcegraph', file: '' },
-        ]))
+    it('return empty file if document has no file format', () =>
+        expect(getParamsFromUriPath('git://github.com/sourcegraph/sourcegraph/testrepo#formatless')).toEqual({
+            repo: 'sourcegraph/sourcegraph',
+            file: '',
+        }))
 })
 
 const projects: Settings['sentry.projects'] = [
@@ -47,14 +49,12 @@ const params = {
 
 describe('matchSentryProject', () => {
     it('extracts repo and file params', () =>
-        expect(matchSentryProject(params, projects)).toEqual([
-            {
-                projectId: '1334031',
-                lineMatches: [
-                    /throw new Error+\(['"]([^'"]+)['"]\)/,
-                    /console\.(warn|debug|info|error|log)\(['"`]([^'"`]+)['"`]\)/,
-                    /log\.(Printf|Print|Println)\(['"]([^'"]+)['"]\)/,
-                ],
-            },
-        ]))
+        expect(matchSentryProject(params, projects)).toEqual({
+            projectId: '1334031',
+            lineMatches: [
+                /throw new Error+\(['"]([^'"]+)['"]\)/,
+                /console\.(warn|debug|info|error|log)\(['"`]([^'"`]+)['"`]\)/,
+                /log\.(Printf|Print|Println)\(['"]([^'"]+)['"]\)/,
+            ],
+        }))
 })
---
+++ src/handler.ts
@@ -0,0 +1,59 @@
+import { Settings } from './settings'
+
+interface Params {
+    repo: string | null
+    file: string | null
+}
+
+interface SentryProject {
+    projectId: string
+    lineMatches: RegExp[]
+}
+
+/**
+ * Extract Sentry params from document URI necessary to
+ * check if the current document sends log events to Sentry and
+ * use these params build URL to the Sentry issues stream page.
+ * @param textDocument A document URI.
+ * @returns repo and file part of URI.
+ */
+export function getParamsFromUriPath(textDocument: string): Params {
+    // TODO: Support more than just GitHub.
+    const repoPattern = /github\.com\/([^\?\#\/]+\/[^\?\#\/]*)/gi
+    const filePattern = /#([^\?\#\/]+)\/.*\.?$/gi
+    const repoM = repoPattern.exec(textDocument)
+    const fileM = filePattern.exec(textDocument)
+    return {
+        repo: repoM ? repoM[1] : '',
+        file: fileM ? fileM[0] : '',
+    }
+}
+
+/**
+ * Verify if the params from the document URI match with the repo and file formats specified
+ * in the Sentry extension settings. If there is a match we know the document is enabled to send logs
+ * to Sentry and can send back the corresponding Sentry project ID.
+ * @param params params extracted from the document's URI.
+ * @param projects Sentry extension projects configurations.
+ * @return Sentry projectID this document reports to.
+ */
+export function matchSentryProject(params: Params, projects: Settings['sentry.projects']): SentryProject | undefined {
+    // Check if a Sentry project is associated with this document's repo and retrieve the project.
+    const project = projects!.find(p => !!new RegExp(p.patternProperties.repoMatch).exec(params.repo!))
+    if (!project) {
+        return
+    }
+
+    // Check if document matches the file matching pattern specified under the Sentry extension configuration.
+    const fileMatched: boolean = !!project.patternProperties.fileMatches.find(
+        pattern => !!new RegExp(pattern).exec(params.file!)
+    )
+    if (!fileMatched) {
+        return
+    }
+
+    return {
+        projectId: project.projectId,
+        lineMatches: project.patternProperties.lineMatches,
+    }
+}
