b959cf61d9ef15514cb23d92c7d138c2a499b650
Vanesa Ortiz <vanesa@sourcegraph.com>
2019-05-15 17:53:55 -0700

    capture group after # in fileMatch regexp pattern

--- src/handler.ts
+++ src/handler.ts
@@ -22,8 +22,7 @@ export function getParamsFromUriPath(textDocumentURI: string): Params {
     // TODO: Support more than just GitHub & Gitlab.
     // TODO: Safeguard for cases where repo/fileMatch are null.
     const repoPattern = /(github\.com|gitlab\.com)\/([^\?\#\/]+\/[^\?\#\/]*)/gi
-    const filePattern = /#(:?.*\.(.*))$/gi
-
+    const filePattern = /#(.*\.(.*))$/gi
     const repoMatch = repoPattern.exec(textDocumentURI)
     const fileMatch = filePattern.exec(textDocumentURI)
     return {
